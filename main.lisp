49f7a0579f31ca4f1a2c6154bf906077efc1ef3e
Arturs Grebstelis <arturs@mikrotik.com>
2022-08-02 17:14:05 +0300

    humble beginnings

---
+++ main.lisp
@@ -0,0 +1,6 @@
+(defstruct cell pos type food bug)
+
+(defstruct bug pos age size)
+
+(format t "Bug Island, inspired by Ellen Ullman's novel `the Bug`~%")
+(quit)
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbcl --noinform --load main.lisp
