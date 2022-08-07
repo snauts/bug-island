9dfa1e66a128c8ec6757798cfe2f3fa2ef13df50
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2022-08-07 13:25:09 +0300

    change shell parameter variable name

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 STEP=${STEP:-1}
 DELAY=${DELAY:-'0.02'}
-FILE=${FILE:-'map.lisp'}
+MAP=${MAP:-'map.lisp'}
 sbcl --noinform --load main.lisp \
-     --eval "(setf *file* \"$FILE\")" \
+     --eval "(setf *file* \"$MAP\")" \
      --eval "(setf *delay* $DELAY)" \
      --eval "(setf *step* $STEP)" \
      --eval "(top-level)" 2> debug.txt
