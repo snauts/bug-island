dc640c19b7369c78d62a24f93ac3641871313752
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2022-08-08 20:24:09 +0300

    fix run script pnm handling

--- run.sh
+++ run.sh
@@ -18,10 +18,12 @@ sbcl --noinform --load main.lisp \
      --eval "(setf *save-picture* $SAVE)" \
      --eval "(top-level)" 2> debug.txt
 
-for F in $(ls *.pnm)
-do
-    convert $F -adaptive-resize 400% $F.gif
-done
+if [ -f pic-00001.pnm ]; then
+    for F in $(ls *.pnm)
+    do
+	convert $F -adaptive-resize 400% $F.gif
+    done
+fi
 
 if [ -f pic-00001.pnm.gif ]; then
     gifsicle --colors 256 --loopcount -d 4 pic*.gif > final.gif
