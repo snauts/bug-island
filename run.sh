c6ff615beabf12501ef1d1f123f64382290df5ce
Arturs Grebstelis <arturs@mikrotik.com>
2022-08-08 17:51:29 +0300

    migration variable for balancing

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ DELAY=${DELAY:-'0.02'}
 MAP=${MAP:-'map.lisp'}
 REGROWTH=${REGROWTH:-'6'}
 LIFESPAN=${LIFESPAN:-'14'}
+MIGRATION=${MIGRATION:-'5'}
 sbcl --noinform --load main.lisp \
      --eval "(setf *file* \"$MAP\")" \
      --eval "(setf *delay* $DELAY)" \
      --eval "(setf *step* $STEP)" \
      --eval "(setf *regrowth* $REGROWTH)" \
      --eval "(setf *lifespan* $LIFESPAN)" \
+     --eval "(setf *low-food* $MIGRATION)" \
      --eval "(top-level)" 2> debug.txt
